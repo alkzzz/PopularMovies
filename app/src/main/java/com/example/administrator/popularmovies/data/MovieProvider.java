8634fec1aa7bf76c8ec0863f760cec35d657da0a
Muhammad Alkaff <m.alkaff9@gmail.com>
2017-07-26 20:57:35 +0800

    movie data persistence

--- app/src/main/java/com/example/administrator/popularmovies/data/MovieProvider.java
+++ app/src/main/java/com/example/administrator/popularmovies/data/MovieProvider.java
@@ -4,6 +4,7 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -29,10 +30,59 @@ public class MovieProvider extends ContentProvider {
         return true;
     }
 
+    @Override
+    public int bulkInsert(@NonNull Uri uri, @NonNull ContentValues[] values) {
+
+        final SQLiteDatabase db = mMovieDbHelper.getReadableDatabase();
+
+        switch (sUriMatcher.match(uri)) {
+            case CODE_MOVIE:
+                db.beginTransaction();
+                int rowInserted = 0;
+                try {
+                    for(ContentValues value: values) {
+                        long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, value);
+                        if (_id != -1) {
+                            rowInserted++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+
+                if (rowInserted > 0) {
+                    getContext().getContentResolver().notifyChange(uri, null);
+                }
+                return rowInserted;
+
+            default:
+                return super.bulkInsert(uri, values);
+        }
+    }
+
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        Cursor cursor;
+
+        switch (sUriMatcher.match(uri)) {
+            case CODE_MOVIE: {
+                cursor = mMovieDbHelper.getReadableDatabase().query(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: "+uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
     @Nullable
